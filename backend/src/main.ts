75d220b2fa3f5ccc9fb6e64bb8cb53359c9dc81b
plinadev <pavlenkopolina415@gmail.com>
2025-08-11 14:23:27 +0300

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from './app.module';
 const allowedOrigin =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:5173'
-    : process.env.FRONTEND_ORIGIN;
+    : 'https://post-it-bbedc.web.app';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
